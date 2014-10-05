958b7390afdd73668528a5e63ef9001a742d32a1
Dave Powers <djpowers89@gmail.com>
2014-10-04 23:22:51 -0400

    Add input fields on form and include on PDF output

--- app.rb
+++ app.rb
@@ -14,7 +14,17 @@ class DeathCertificateGenerator < Sinatra::Base
     content_type 'application/pdf'
 
     pdf = Prawn::Document.new
-    pdf.text params[:certificate][:name]
+    pdf.text "Name of Deceased: #{params[:certificate][:name]}"
+    pdf.text "Place of Death: #{params[:certificate][:place_of_death]}"
+    pdf.text "Sex: #{params[:certificate][:sex]}"
+    pdf.text "Color or Race: #{params[:certificate][:race]}"
+    pdf.text "Marital Status: #{params[:certificate][:marital_stats]}"
+    pdf.text "Date of Birth: #{params[:certificate][:date_of_birth]}"
+    pdf.text "Occupation: #{params[:certificate][:occupation]}"
+    pdf.text "Birthplace: #{params[:certificate][:birthplace]}"
+    pdf.text "Date of Death: #{params[:certificate][:date_of_death]}"
+    pdf.text "Place of Burial: #{params[:certificate][:place_of_burial]}"
+    pdf.text "Undertaker: #{params[:certificate][:undertaker]}"
     pdf.render
   end
 end

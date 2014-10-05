1ce9ceb4f277cf0179a2e5786ea902ced8cac322
Dave Powers <djpowers89@gmail.com>
2014-10-04 21:28:12 -0400

    Create form which displayed input on PDF upon submit

--- app.rb
+++ app.rb
@@ -1,6 +1,20 @@
 require 'sinatra'
+require 'prawn'
 
 class DeathCertificateGenerator < Sinatra::Base
+  configure :development do
+    require 'pry'
+  end
+
   get '/' do
+    erb :index
+  end
+
+  post '/certificate' do
+    content_type 'application/pdf'
+
+    pdf = Prawn::Document.new
+    pdf.text params[:certificate][:name]
+    pdf.render
   end
 end

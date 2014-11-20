959c45ea51f9a7d52127a8a734835571129ab26b
Dave Powers <djpowers89@gmail.com>
2014-11-19 22:05:49 -0500

    Add method to truncate text to prevent form fields from wrapping outside bounding box. Fixes #1.

--- app.rb
+++ app.rb
@@ -38,7 +38,7 @@ class DeathCertificateGenerator < Sinatra::Base
     pdf.image "app/images/Guilloche-Seal.png"
     pdf.float do
       pdf.move_up 100
-      pdf.text "#{set_state_name(params[:certificate][:place_of_death])}".upcase, align: :center, size: 32
+      pdf.text "#{set_state_name(truncate(params[:certificate][:place_of_death]))}".upcase, align: :center, size: 32
       pdf.text 'State Board of Health'.upcase, align: :center, size: 24
       pdf.text 'Certificate of Death'.upcase, align: :center, size: 24
     end
@@ -46,47 +46,47 @@ class DeathCertificateGenerator < Sinatra::Base
     pdf.text_box "File No.\n#{Time.now.to_i}", at: [600, 500]
     # pdf.stroke_axis
     pdf.bounding_box([0, 400], width: 233, height: 50) do
-      pdf.text "01. #{'Place of Death'.upcase}:\n<u>#{set_city_name(params[:certificate][:place_of_death])}</u>", inline_format: true
+      pdf.text "01. #{'Place of Death'.upcase}:\n<u>#{set_city_name(truncate(params[:certificate][:place_of_death]))}</u>", inline_format: true
       stroke_bounds(pdf)
     end
     pdf.bounding_box([233, 400], width: 233, height: 50) do
-      pdf.text "02. #{'Name of Deceased'.upcase}:\n<u>#{params[:certificate][:name]}</u>", inline_format: true
+      pdf.text "02. #{'Name of Deceased'.upcase}:\n<u>#{truncate(params[:certificate][:name])}</u>", inline_format: true
       stroke_bounds(pdf)
     end
     pdf.bounding_box([466, 400], width: 233, height: 50) do
-      pdf.text "03. #{'Sex'.upcase}:\n<u>#{params[:certificate][:sex]}</u>", inline_format: true
+      pdf.text "03. #{'Sex'.upcase}:\n<u>#{truncate(params[:certificate][:sex])}</u>", inline_format: true
       stroke_bounds(pdf)
     end
     pdf.bounding_box([0, 350], width: 233, height: 50) do
-      pdf.text "04. #{'Color or Race'.upcase}:\n<u>#{params[:certificate][:race]}</u>", inline_format: true
+      pdf.text "04. #{'Color or Race'.upcase}:\n<u>#{truncate(params[:certificate][:race])}</u>", inline_format: true
       stroke_bounds(pdf)
     end
     pdf.bounding_box([233, 350], width: 233, height: 50) do
-      pdf.text "05. #{'Marital Status'.upcase}:\n<u>#{params[:certificate][:marital_status]}</u>", inline_format: true
+      pdf.text "05. #{'Marital Status'.upcase}:\n<u>#{truncate(params[:certificate][:marital_status])}</u>", inline_format: true
       stroke_bounds(pdf)
     end
     pdf.bounding_box([466, 350], width: 233, height: 50) do
-      pdf.text "06. #{'Date of Birth'.upcase}:\n<u>#{params[:certificate][:date_of_birth]}</u>", inline_format: true
+      pdf.text "06. #{'Date of Birth'.upcase}:\n<u>#{truncate(params[:certificate][:date_of_birth])}</u>", inline_format: true
       stroke_bounds(pdf)
     end
     pdf.bounding_box([0, 300], width: 233, height: 50) do
-      pdf.text "07. #{'Occupation'.upcase}:\n<u>#{set_optional_field(params[:certificate][:occupation])}</u>", inline_format: true
+      pdf.text "07. #{'Occupation'.upcase}:\n<u>#{set_optional_field(truncate(params[:certificate][:occupation]))}</u>", inline_format: true
       stroke_bounds(pdf)
     end
     pdf.bounding_box([233, 300], width: 233, height: 50) do
-      pdf.text "08. #{'Birthplace'.upcase}:\n<u>#{set_city_name(params[:certificate][:birthplace])}</u>", inline_format: true
+      pdf.text "08. #{'Birthplace'.upcase}:\n<u>#{set_city_name(truncate(params[:certificate][:birthplace]))}</u>", inline_format: true
       stroke_bounds(pdf)
     end
     pdf.bounding_box([466, 300], width: 233, height: 50) do
-      pdf.text "09. #{'Date of Death'.upcase}:\n<u>#{params[:certificate][:date_of_death]}</u>", inline_format: true
+      pdf.text "09. #{'Date of Death'.upcase}:\n<u>#{truncate(params[:certificate][:date_of_death])}</u>", inline_format: true
       stroke_bounds(pdf)
     end
     pdf.bounding_box([0, 250], width: 233, height: 50) do
-      pdf.text "10. #{'Place of Burial'.upcase}:\n<u>#{set_optional_field(params[:certificate][:place_of_burial])}</u>", inline_format: true
+      pdf.text "10. #{'Place of Burial'.upcase}:\n<u>#{set_optional_field(truncate(params[:certificate][:place_of_burial]))}</u>", inline_format: true
       stroke_bounds(pdf)
     end
     pdf.bounding_box([233, 250], width: 233, height: 50) do
-      pdf.text "11. #{'Undertaker'.upcase}:\n<u>#{set_optional_field(params[:certificate][:undertaker])}</u>", inline_format: true
+      pdf.text "11. #{'Undertaker'.upcase}:\n<u>#{set_optional_field(truncate(params[:certificate][:undertaker]))}</u>", inline_format: true
       stroke_bounds(pdf)
     end
     pdf.image "app/images/stock-photo-approved-stamp.png", at: [500, 200]
@@ -115,6 +115,14 @@ class DeathCertificateGenerator < Sinatra::Base
 
   def set_optional_field(input)
     input.empty? ? 'Unknown' : input
+  end
+
+  def truncate(text)
+    if text.length > 20
+      "#{text[0..17]}..."
+    else
+      text
+    end
   end
 
   def state_mappings
